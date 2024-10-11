3c9fbe9d46c931211984cc4eb0986b1ee26ce17b
Reshma Menon R <pct3kd@virginia.edu>
2024-10-11 13:47:21 -0500

    Created this .py file in vim.

---
+++ python_analysis/configs/sample_configs/2022bkgdetails.py
@@ -0,0 +1,23 @@
+import json
+
+# Data to be written into JSON
+data =[
+        {
+    "name": "0000",
+    "location": "/uscms/home/reshmar/nobackup/CMSSW_13_0_13/src/iDMe/AODSkimmer/Final_DYJets_2022_output.root",
+    "type": "bkg",
+    "year": 2022,
+    "xsec": 10,
+    "nFile": 1,
+    "blacklist": [],
+    "sum_wgt": 1000,
+    "num_events": 1000,
+    "nFiles": 1
+}
+]
+# Write data to JSON file
+with open('Final2022bkg.json', 'w') as json_file:
+    json.dump(data, json_file, indent=4)
+
+print("JSON file created successfully!")
+
