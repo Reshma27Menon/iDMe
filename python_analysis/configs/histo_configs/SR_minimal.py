20c52172c74037fa582134bd8bffd67cbc49dd53
Reshma Menon R <pct3kd@virginia.edu>
2024-10-20 12:07:36 -0500

    Commented out existing variables and added ele_pt as the only variable to minimize the load on the processor.

--- python_analysis/configs/histo_configs/SR_minimal.py
+++ python_analysis/configs/histo_configs/SR_minimal.py
@@ -7,25 +7,27 @@ import awkward as ak
 def make_histograms():
     histograms = {
         # quantities associated w/ selected vertex
-        "bdtScore" : Hist(samp,cut,bdtScore,storage=hist.storage.Weight()),
-        "gen_met_noWgt" :  Hist(samp,cut,met,storage=hist.storage.Weight()),
-        "gen_met" :  Hist(samp,cut,met,storage=hist.storage.Weight()),
-        "gen_leadjet_pt" : Hist(samp,cut,jet_pt,storage=hist.storage.Weight()),
-        "gen_leadjet_pt_noWgt" : Hist(samp,cut,jet_pt,storage=hist.storage.Weight())
+        #"bdtScore" : Hist(samp,cut,bdtScore,storage=hist.storage.Weight()),
+        #"gen_met_noWgt" :  Hist(samp,cut,met,storage=hist.storage.Weight()),
+        #"gen_met" :  Hist(samp,cut,met,storage=hist.storage.Weight()),
+        #"gen_leadjet_pt" : Hist(samp,cut,jet_pt,storage=hist.storage.Weight()),
+        #"gen_leadjet_pt_noWgt" : Hist(samp,cut,jet_pt,storage=hist.storage.Weight())
+        "sel_vtx_pt" : Hist(samp,cut,ele_pt,storage=hist.storage.Weight())
+        #"sel_vtx_eta" : Hist(samp,cut,ele_eta,storage=hist.storage.Weight())
 
     }
+    print ("type:",type(histograms))
     return histograms
 
 subroutines = []
 
 def fillHistos(events,histos,samp,cut,info,sum_wgt=1):
+    
+    e1 = events.sel_vtx.e1
+    e2 = events.sel_vtx.e2
+    min_dxy = np.minimum(np.abs(e1.dxy),np.abs(e2.dxy))
+    max_pfiso = ak.where(e1.PFRelIso<e2.PFRelIso,e2.PFRelIso,e1.PFRelIso)
     wgt = events.eventWgt/sum_wgt
-    if 'BDTScore' in events.fields:
-        histos["bdtScore"].fill(samp=samp,cut=cut,score=events.BDTScore,weight=wgt)
-    if info['type'] == "signal":
-        histos['gen_met_noWgt'].fill(samp=samp,cut=cut,met=events.GenMET.pt,weight=1)
-        histos['gen_met'].fill(samp=samp,cut=cut,met=events.GenMET.pt,weight=wgt)
-        histos['gen_leadjet_pt'].fill(samp=samp,cut=cut,pt=events.GenJet.pt[:,0],weight=wgt)
-        histos['gen_leadjet_pt_noWgt'].fill(samp=samp,cut=cut,pt=events.GenJet.pt[:,0],weight=1)
-
-
+    vtx = events.sel_vtx
+    histos["sel_vtx_pt"].fill(samp=samp,cut=cut,pt=vtx.pt,weight=wgt)
+    #histos["sel_vtx_eta"].fill(samp=samp,cut=cut,eta=vtx.eta,weight=wgt)
