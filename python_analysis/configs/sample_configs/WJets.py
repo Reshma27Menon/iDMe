0e953338b21c36452adc3aef14045b478fab264f
Reshma Menon R <pct3kd@virginia.edu>
2024-11-03 10:20:50 -0600

    Created JSON file for 2022 WJets sample

---
+++ python_analysis/configs/sample_configs/WJets.py
@@ -0,0 +1,21 @@
+import json
+
+
+data =[
+        {
+    "name": "WJets",
+    "location": "/uscms/home/reshmar/nobackup/CMSSW_13_0_13/src/iDMe/AODSkimmer/WJets_2022_output.root",
+    "type": "bkg",
+    "year": 2022,
+    "xsec": 10,
+    "nFile": 1,
+    "blacklist": [],
+    "nFiles": 1
+}
+]
+# Write data to JSON file
+with open('WJets2022.json', 'w') as json_file:
+    json.dump(data, json_file, indent=4)
+
+print("JSON file created successfully!")
+
