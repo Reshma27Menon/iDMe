d573e92218f8f1f8d95356fef17195f55879ebfd
Reshma Menon R <pct3kd@virginia.edu>
2024-12-03 08:33:54 -0600

    Modified the try-except statement as it was unable to open the root file content (need a cross check)

--- python_analysis/configs/sample_configs/findBadNtuples.py
+++ python_analysis/configs/sample_configs/findBadNtuples.py
@@ -1,3 +1,4 @@
+#It took me 3 hours to run this script. Need to figure out why!
 import uproot
 import numpy as np
 import json
@@ -9,24 +10,47 @@ import multiprocessing as mp
 from ROOT import TTree, TFile
 
 def checkFile(ntuple):
-    #try:
     good = True
-    with TFile.Open(ntuple,"read") as f:
-        t = f.Get("ntuples/outT")
-        if not t.GetTree():
-            print("bad tree!")
-            good = False
-        if good:
-            for i in range(0,t.GetEntries()):
-                error = t.GetEntry(i)
-                if error < 0:
-                    print("bad event!")
-                    good = False
-                    break
+    f = TFile.Open(ntuple, "read")  # Open the ROOT file
+    if not f or f.IsZombie():
+        print("File could not be opened or is corrupted!")
+        return False  # File is invalid
+    t = f.Get("ntuples/outT")
+    if not t or not t.GetTree():
+        print("bad tree!")
+        good = False
+    if good:
+        for i in range(0, t.GetEntries()):
+            error = t.GetEntry(i)
+            if error < 0:
+                print("bad event!")
+                good = False
+                break
+    f.Close()  # Manually close the file
     return good
-    #except:
+
+######OLD CODE############################
+#def checkFile(ntuple):
+#    #try:
+#    good = True
+#    with TFile.Open(ntuple,"read") as f:
+#        t = f.Get("ntuples/outT")
+#        if not t.GetTree():
+#            print("bad tree!")
+#            good = False
+#        if good:
+#            for i in range(0,t.GetEntries()):
+#                error = t.GetEntry(i)
+#                if error < 0:
+#                    print("bad event!")
+#                    good = False
+#                    break
+#    return good
+#    print ("good:", good)
+#    #except:
     #    print("exception!")
     #    return False
+############################################
 
 inputJson = sys.argv[1]
 with open(inputJson) as f:
@@ -45,16 +69,20 @@ for samp in samples:
         xrdClient = client.FileSystem("root://cmseos.fnal.gov")
         if type(loc) != list:
             status, flist = xrdClient.dirlist(loc)
-            fullList = ["root://cmsxrootd.fnal.gov/"+loc+"/"+item.name for item in flist if '.root' in item.name]
+            print ("status:", status)
+            fullList = ["root://cmseos.fnal.gov/"+loc+item.name for item in flist if '.root' in item.name]
+            print ("length of fullList:", len(fullList))
         else:
             fullList = []
             for l in loc:
                 status, flist = xrdClient.dirlist(l)
-                fullList.extend(["root://cmsxrootd.fnal.gov/"+l+"/"+item.name for item in flist if '.root' in item.name])
+                fullList.extend(["root://cmseos.fnal.gov/"+l+"/"+item.name for item in flist if '.root' in item.name])
+         
         for f in tqdm(fullList):
             if not checkFile(f):
                 blacklist.append(f)
-
+    #for p in fullList:
+            # print ("check..check:",checkFile(p))
     print('Blacklisted {0} files in {1}'.format(len(blacklist),samp['name']))
     samp['blacklist'] = blacklist
 
