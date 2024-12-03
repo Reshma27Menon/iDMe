d2e7214683b12682848b9048a1d30d22132c4da2
Reshma Menon R <pct3kd@virginia.edu>
2024-12-03 11:47:54 -0600

    Script to open a root file using uproot

--- python_analysis/configs/sample_configs/up.py
+++ python_analysis/configs/sample_configs/up.py
@@ -5,6 +5,7 @@ import uproot
 
 file_path = "root://cmseos.fnal.gov//eos/uscms//store/group/lpcmetx/iDMe/Samples/Ntuples/background_WJets12022/2022/WJets/WJetsToLNu1/WtoLNu-4Jets_TuneCP5_13p6TeV_madgraphMLM-pythia8/crab_iDMe_WJetsToLNu1_2024_11_14-08_19/241114_142230/0000/WJetsToLNu1_1-1.root"
 
+
 #file_path = "root://cmsxrootd.fnal.gov//eos/uscms//store/group/lpcmetx/iDMe/Samples/Ntuples/background_WJets12022/2022/WJets/WJetsToLNu1/WtoLNu-4Jets_TuneCP5_13p6TeV_madgraphMLM-pythia8/crab_iDMe_WJetsToLNu1_2024_11_14-08_19/241115_142230/0000/WJetsToLNu1_1-1.root"
 
 # Open the ROOT file
