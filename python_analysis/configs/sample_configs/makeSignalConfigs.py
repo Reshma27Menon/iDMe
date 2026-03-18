084489d83d0a81ca2592711274bae655d3772bde
Alaa A. Abdelhamid <130788379+A-A-Abdelhamid@users.noreply.github.com>
2026-03-18 13:17:11 -0400

    Cleaning #

--- python_analysis/configs/sample_configs/makeSignalConfigs.py
+++ python_analysis/configs/sample_configs/makeSignalConfigs.py
@@ -210,50 +210,50 @@ xrdClient = client.FileSystem("root://cmseos.fnal.gov")
 #     #             else:
 #     #                 info["name"] = "sig_Mchi-{0}_dMchi-{1}_ct-{2}".format(info["Mchi"],info["dMchi"],info["ctau"])
 #     #             info["sum_wgt"] = 0.0
-# #     #             info["type"] = "signal"
-# #     #             info["year"] = int(year)
-# #     #             info["alphaD"] = alpha
-# #     #             info["xsec"] = 0.0
-# #     #             rootFiles = [rf.name for rf in xrdClient.dirlist(info["location"])[1] if '.root' in rf.name]
-# #     #             info["nFiles"] = len(rootFiles)
-# #     #             output.append(info)
+#     #             info["type"] = "signal"
+#     #             info["year"] = int(year)
+#     #             info["alphaD"] = alpha
+#     #             info["xsec"] = 0.0
+#     #             rootFiles = [rf.name for rf in xrdClient.dirlist(info["location"])[1] if '.root' in rf.name]
+#     #             info["nFiles"] = len(rootFiles)
+#     #             output.append(info)
 
-# #     # if skimmed:
+#     # if skimmed:
 
-# #     #     out_json = "skimmed_signal_{0}_{1}_{2}.json".format(year,name,alpha)
-# #     # else:
-# #     #     out_json = "signal_{0}_{1}_{2}.json".format(year,name,alpha)
-# #     # with open(out_json,"w") as outfile:
-# #     #     json.dump(output,outfile,indent=4)
-# # elif mode == "bkg":
-# #     print ("Hello")
+#     #     out_json = "skimmed_signal_{0}_{1}_{2}.json".format(year,name,alpha)
+#     # else:
+#     #     out_json = "signal_{0}_{1}_{2}.json".format(year,name,alpha)
+#     # with open(out_json,"w") as outfile:
+#     #     json.dump(output,outfile,indent=4)
+# elif mode == "bkg":
+#     print ("Hello")
     
-# #     if skimmed:
-# #         status,bkgs = xrdClient.dirlist(f"{prefix}/")
-# #         print ("Hello again")
-# #         print ("status:", status)
-# #     else:
-# #         status, bkgs = xrdClient.dirlist(f"{prefix}/{year}/")
+#     if skimmed:
+#         status,bkgs = xrdClient.dirlist(f"{prefix}/")
+#         print ("Hello again")
+#         print ("status:", status)
+#     else:
+#         status, bkgs = xrdClient.dirlist(f"{prefix}/{year}/")
 
     
     
-# #     bkgs = [bkg.name for bkg in bkgs]
-# #     output = []
-# #     for bkg in bkgs:
-# #         if skimmed:
-# #             base_dir = f"{prefix}/{bkg}"
-# #             subsamples = [bkg]
+#     bkgs = [bkg.name for bkg in bkgs]
+#     output = []
+#     for bkg in bkgs:
+#         if skimmed:
+#             base_dir = f"{prefix}/{bkg}"
+#             subsamples = [bkg]
             
-# #         else:
-# #             base_dir = f"{prefix}/{year}/{bkg}"
-# #             subsamples = [d.name for d in xrdClient.dirlist(base_dir)[1]]
+#         else:
+#             base_dir = f"{prefix}/{year}/{bkg}"
+#             subsamples = [d.name for d in xrdClient.dirlist(base_dir)[1]]
             
-# #         for subsample in subsamples:
-# #             if skimmed:
-# #                 target_dir = base_dir
-# #                 print ("target_dir:", target_dir)
-# #                 full_path = f"{target_dir}/*.root"
-# #                 print("Check....check:",full_path)
+#         for subsample in subsamples:
+#             if skimmed:
+#                 target_dir = base_dir
+#                 print ("target_dir:", target_dir)
+#                 full_path = f"{target_dir}/*.root"
+#                 print("Check....check:",full_path)
                 
 
 # #             else:
@@ -261,32 +261,32 @@ xrdClient = client.FileSystem("root://cmseos.fnal.gov")
                 
                 
                 
-# #             rootFiles = [ f for f in glob.glob(f"{target_dir}/**/*.root", recursive=True) ]   
+#             rootFiles = [ f for f in glob.glob(f"{target_dir}/**/*.root", recursive=True) ]   
                       
-# #             rootFiles = [r for r in rootFiles if '.root' in r]
-# #             print ("rootFiles:")
-# #             for f in rootFiles:
-# #                 print (f)
+#             rootFiles = [r for r in rootFiles if '.root' in r]
+#             print ("rootFiles:")
+#             for f in rootFiles:
+#                 print (f)
             
-# #             fileDirs = ["/".join(f.split("/")[:-1])+"/" for f in rootFiles]
-# #             fileDirs = list(set(fileDirs)) # list of unique file directories
+#             fileDirs = ["/".join(f.split("/")[:-1])+"/" for f in rootFiles]
+#             fileDirs = list(set(fileDirs)) # list of unique file directories
             
             
             
-# #             info = {}
-# #             if skimmed:
-# #                 info["name"] = subsample.replace("output_","")
-# #             else:
-# #                 info["name"] = f"{bkg}_{subsample}"
-# #             info["location"] = fileDirs[0] if len(fileDirs) == 1 else fileDirs
-# #             info["type"] = "bkg"
-# #             info["year"] = int(year)
-# #             nFiles=0
-# #             for fdir in fileDirs:
-# #                 nFiles += len([rf.name for rf in xrdClient.dirlist(fdir)[1] if '.root' in rf.name])
-# #             info["nFiles"] = nFiles
-# #             if nFiles == 0:
-# #                 continue
+#             info = {}
+#             if skimmed:
+#                 info["name"] = subsample.replace("output_","")
+#             else:
+#                 info["name"] = f"{bkg}_{subsample}"
+#             info["location"] = fileDirs[0] if len(fileDirs) == 1 else fileDirs
+#             info["type"] = "bkg"
+#             info["year"] = int(year)
+#             nFiles=0
+#             for fdir in fileDirs:
+#                 nFiles += len([rf.name for rf in xrdClient.dirlist(fdir)[1] if '.root' in rf.name])
+#             info["nFiles"] = nFiles
+#             if nFiles == 0:
+#                 continue
 
 # #             if skimmed:
 # #                 samp_name = subsample.replace("output_","")
@@ -294,32 +294,32 @@ xrdClient = client.FileSystem("root://cmseos.fnal.gov")
 # #                 info['xsec'] = ref_info[samp_name]['xsec']
 # #                 info['blacklist'] = ref_info[samp_name]['blacklist']
 # #             else:
-# #                 info["sum_wgt"] = 0.0
-# #                 info["xsec"] = 0.0
+#                 info["sum_wgt"] = 0.0
+#                 info["xsec"] = 0.0
 
-# #             output.append(info)
-# #             print ("info:", info)
+#             output.append(info)
+#             print ("info:", info)
 
-# #     if skimmed:
-# #         out_json = "skimmed_bkg_{0}_{1}.json".format(year,name)
-# #     else:
-# #         out_json = "bkg_{0}_{1}.json".format(year,name)
-# #     with open(out_json,"w") as outfile:
-# #         json.dump(output,outfile,indent=4)
-# # elif mode == "data":
-# #     if skimmed:
-# #         status, samples = xrdClient.dirlist(f"{prefix}/")
-# #     else:
-# #         status, samples = xrdClient.dirlist(f"{prefix}/{year}/")
-# #     samples = [samp.name for samp in samples]
-# #     output = []
-# #     for samp in samples:
-# #         if skimmed:
-# #             base_dir = f"{prefix}/{samp}"
-# #             subsamples = [samp]
-# #         else:
-# #             base_dir = f"{prefix}/{year}/{samp}"
-# # # =======
+#     if skimmed:
+#         out_json = "skimmed_bkg_{0}_{1}.json".format(year,name)
+#     else:
+#         out_json = "bkg_{0}_{1}.json".format(year,name)
+#     with open(out_json,"w") as outfile:
+#         json.dump(output,outfile,indent=4)
+# elif mode == "data":
+#     if skimmed:
+#         status, samples = xrdClient.dirlist(f"{prefix}/")
+#     else:
+#         status, samples = xrdClient.dirlist(f"{prefix}/{year}/")
+#     samples = [samp.name for samp in samples]
+#     output = []
+#     for samp in samples:
+#         if skimmed:
+#             base_dir = f"{prefix}/{samp}"
+#             subsamples = [samp]
+#         else:
+#             base_dir = f"{prefix}/{year}/{samp}"
+# # =======
 if mode == "sig":
     if skimmed:
         status, points = xrdClient.dirlist(f"{prefix}/")
@@ -386,40 +386,40 @@ elif mode == "bkg":
             subsamples = [bkg]
         else:
             base_dir = f"{prefix}/{year}/{bkg}"
-# # >>>>>>> kyungmin/main
+# >>>>>>> kyungmin/main
             subsamples = [d.name for d in xrdClient.dirlist(base_dir)[1]]
         for subsample in subsamples:
             if skimmed:
                 target_dir = base_dir
             else:
                 target_dir = f"{base_dir}/{subsample}/"
-# # <<<<<<< HEAD
+# <<<<<<< HEAD
 #             rootFiles = subprocess.run(['eos','root://cmseos.fnal.gov/','find','-name','*.root','-f',target_dir],stdout=subprocess.PIPE).stdout.decode('utf-8').splitlines()
 #             rootFiles = [r for r in rootFiles if '.root' in r]
 #             fileDirs = ["/".join(f.split("/")[:-1])+"/" for f in rootFiles]
-# # =======
+# =======
             #rootFiles = subprocess.run(['eos','root://cmseos.fnal.gov/','find','-name','*.root','-f',target_dir],stdout=subprocess.PIPE).stdout.decode('utf-8').splitlines()
             #rootFiles = [r for r in rootFiles if '.root' in r]
             rootFiles = [ f for f in glob.glob(f"/eos/uscms/{target_dir}/**/*.root", recursive=True) ]
             fileDirs = ["/".join(f.split("/")[:-1])+"/" for f in rootFiles]
             fileDirs = [ d.split("/eos/uscms/")[-1] for d in fileDirs]
-# # >>>>>>> kyungmin/main
+# >>>>>>> kyungmin/main
             fileDirs = list(set(fileDirs)) # list of unique file directories
             
             info = {}
             if skimmed:
                 info["name"] = subsample.replace("output_","")
             else:
-# # <<<<<<< HEAD
-# #                 info["name"] = f"{samp}_{subsample}"
-# #             info["location"] = fileDirs[0] if len(fileDirs) == 1 else fileDirs
-# #             info["sum_wgt"] = 0.0
-# #             info["type"] = "data"
-# # =======
+# <<<<<<< HEAD
+#                 info["name"] = f"{samp}_{subsample}"
+#             info["location"] = fileDirs[0] if len(fileDirs) == 1 else fileDirs
+#             info["sum_wgt"] = 0.0
+#             info["type"] = "data"
+# =======
                 info["name"] = f"{bkg}_{subsample}"
             info["location"] = fileDirs[0] if len(fileDirs) == 1 else fileDirs
             info["type"] = "bkg"
-# # >>>>>>> kyungmin/main
+# >>>>>>> kyungmin/main
             info["year"] = int(year)
             nFiles=0
             for fdir in fileDirs:
